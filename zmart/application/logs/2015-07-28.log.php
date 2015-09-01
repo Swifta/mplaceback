44a236448076ff295f2233e27a958576d00f4aba
dupsykool <ladmodupe@gmail.com>
2015-09-01 13:14:14 +0100

    commit new code from ndot

---
+++ zmart/application/logs/2015-07-28.log.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2015-07-28 15:22:08 +05:30 --- error: Uncaught Kohana_Exception: The requested view, _error/404, could not be found in file C:/wamp/www/Emarketplace/trunk/system/core/Kohana.php on line 1162
+2015-07-28 17:57:53 +05:30 --- error: Uncaught Kohana_Exception: The requested view, _error/404, could not be found in file C:/wamp/www/Emarketplace/trunk/system/core/Kohana.php on line 1162
+2015-07-28 17:57:58 +05:30 --- error: Uncaught Kohana_Exception: The requested view, _error/404, could not be found in file C:/wamp/www/Emarketplace/trunk/system/core/Kohana.php on line 1162
+2015-07-28 17:57:59 +05:30 --- error: Uncaught Kohana_Exception: The requested view, _error/404, could not be found in file C:/wamp/www/Emarketplace/trunk/system/core/Kohana.php on line 1162
+2015-07-28 17:58:02 +05:30 --- error: Uncaught Kohana_Exception: The requested view, _error/404, could not be found in file C:/wamp/www/Emarketplace/trunk/system/core/Kohana.php on line 1162
+2015-07-28 19:50:20 +05:30 --- error: Uncaught Kohana_Exception: The requested view, _error/404, could not be found in file C:/wamp/www/Emarketplace/trunk/system/core/Kohana.php on line 1162
+2015-07-28 19:50:20 +05:30 --- error: Uncaught Kohana_Exception: The requested view, _error/404, could not be found in file C:/wamp/www/Emarketplace/trunk/system/core/Kohana.php on line 1162
---
+++ zmart/application/logs/2015-09-01.log.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2015-09-01 04:03:16 +05:30 --- error: Uncaught PHP Error: mysql_connect() [<a href='function.mysql-connect'>function.mysql-connect</a>]: Access denied for user 'root'@'localhost' (using password: YES) in file system/libraries/drivers/Database/Mysql.php on line 61
